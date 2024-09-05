97593a763393c4298830d56907ed2190b5454113
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2024-09-04 22:12:18 -0300

    Corrigindo nome de arquivo

---
+++ en-US/commands/roleplay/laugh.js
@@ -0,0 +1,19 @@
+export default {
+    content: {
+        self: "Turned into the Joker, laughing alone",
+        bot: "Why are you laughing at me??!!"
+    },
+    embeds: [
+        {
+            title: "Laugh",
+            description: "{var1} laughed at {var2}"
+        }
+    ],
+    components: {
+        buttons: [
+            {
+                label: "Return the favor",
+            }
+        ]
+    }
+}
